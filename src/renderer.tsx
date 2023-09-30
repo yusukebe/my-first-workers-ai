27aa4a7286d46b573b79fba24a75da0274fb2ba3
Yusuke Wada <yusuke@kamawada.com>
2023-09-30 22:54:02 +0900

    make it as a starter

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -6,13 +6,8 @@ export const renderer = (c: Context) => (content: string) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css" />
-        <script src="/script.js"></script>
       </head>
       <body>
-        <header>
-          <h1>My first Workers AI</h1>
-        </header>
         <div>{content}</div>
       </body>
     </html>
