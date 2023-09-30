b40abe3b7670341e69ca1aaa2277328837b2ae02
Yusuke Wada <yusuke@kamawada.com>
2023-09-30 18:37:12 +0900

    renew!

---
+++ src/index.tsx
@@ -0,0 +1,72 @@
+import { Hono } from 'hono'
+import { renderer } from './renderer'
+import { Ai } from '@cloudflare/ai'
+import script from '../assets/script.js'
+
+type Bindings = {
+  AI: any
+}
+
+type Answer = {
+  response: string
+}
+
+type Message = {
+  content: string
+  role: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/script.js', (c) => {
+  return c.body(script, 200, {
+    'Content-Type': 'text/javascript'
+  })
+})
+
+app.get('*', async (c, next) => {
+  c.setRenderer(renderer(c))
+  await next()
+})
+
+app.get('/', (c) => {
+  return c.render(
+    <>
+      <h2>You</h2>
+      <form id="input-form" autocomplete="off" method="post">
+        <input
+          type="text"
+          name="query"
+          style={{
+            width: '100%'
+          }}
+        />
+        <button type="submit">Send</button>
+      </form>
+      <h2>AI</h2>
+      <pre
+        id="ai-content"
+        style={{
+          'white-space': 'pre-wrap'
+        }}
+      ></pre>
+    </>
+  )
+})
+
+app.post('/ai', async (c) => {
+  const { messages } = await c.req.json<{ messages: Message[] }>()
+  const ai = new Ai(c.env.AI)
+  const answer: Answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+    messages
+  })
+  const strings = [...answer.response]
+  return c.streamText(async (stream) => {
+    for (const s of strings) {
+      stream.write(s)
+      await stream.sleep(10)
+    }
+  })
+})
+
+export default app
