20f6563361d1eb7cba91d3602f0e6765b3ca3abe
Yusuke Wada <yusuke@kamawada.com>
2023-10-06 17:32:27 +0900

    remove css import

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,7 +5,6 @@ export const renderer = jsxRenderer(({ children }) => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css" />
       <script src="/script.js"></script>
     </head>
     <body>
